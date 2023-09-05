e04f3934eb64fbf218df8719c171ac3f8fa5103f
Andy Koi <anhkhoi1192002@gmail.com>
2023-09-05 16:59:13 +0700

    Khoi: add sms

--- Customer/src/controllers/paymentController.ts
+++ Customer/src/controllers/paymentController.ts
@@ -2,10 +2,24 @@ import express from 'express'
 import { catchAsync } from '~/utils/catchAsync'
 import PaymentModel from '~/models/PaymentModel'
 import { PaymentModelBuilder } from '~/services/PaymentService/PaymentBuilder'
+import { Twilio } from 'twilio'
 
 export default {
   getPayments: catchAsync(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
+      const accountSid = 'AC530d5e90d802ddfd1b9e32e6fe476fee'
+      const authToken = '8a0aba1fdf5b462aac8f73dfe829e3e7'
+      const client = new Twilio(accountSid, authToken)
+
+      client.messages
+        .create({
+          from: '+12565884188',
+          to: '+84377023495',
+          body: 'Con Chó Chính  '
+        })
+        .then((message) => console.log(message.sid))
+        .catch((error) => console.log(error))
+
       const payments = await PaymentModel.find({}).exec()
       res.status(200).json({
         status: 'success',
